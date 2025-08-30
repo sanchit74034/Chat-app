941efbb86411ec7fb278e6e4bfac5f6499ab26ca
Sanchit Mittal <your-sanchitjc821@gmail.com>
2025-08-30 13:39:32 +0530

    Fixed bug

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+import.meta.env.MODE === "development" 
+? "http://localhost:5001" 
+: "https://chat-app-o7hz.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
