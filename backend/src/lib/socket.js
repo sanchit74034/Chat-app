8d362da8cc87292f1e17b22d20d8c32956856f17
Sanchit Mittal <your-sanchitjc821@gmail.com>
2025-08-30 13:48:08 +0530

    Fixed bug again

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://chat-app-1-zpc6.onrender.com"],
     credentials: true,
   },
 });
